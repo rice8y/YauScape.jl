e2b6a720d6cf60388016213847d3f75cf722ed06
rice8y <eitoy1128@mbr.nifty.com>
2025-03-08 03:20:18 +0900

    fix src/YauScape.jl

--- src/YauScape.jl
+++ src/YauScape.jl
@@ -3,63 +3,61 @@ module YauScape
 using Plots
 gr()
 
-export calabi_yau
+export calabi_yau, calabi_yau_anim
 
-function u1(a, b)
-    return 0.5 * (exp(a + im * b) + exp(-a - im * b))
+function u1(a::Float64, b::Float64)
+    0.5 * (exp(a + im * b) + exp(-a - im * b))
 end
 
-function u3(a, b)
-    return 0.5 * (exp(a + im * b) - exp(-a - im * b))
+function u3(a::Float64, b::Float64)
+    0.5 * (exp(a + im * b) - exp(-a - im * b))
 end
 
-function calabi_yau(n, a)
-    dx = π / 100
-    dy = π / 100
-    x_grid = 0:dx:π/2
-    y_grid = -π/2:dy:π/2
-    nx, ny = length(x_grid), length(y_grid)
-    
-    U1_pow = [u1(x, y)^(2/n) for x in x_grid, y in y_grid]
-    U3_pow = [u3(x, y)^(2/n) for x in x_grid, y in y_grid]
-    
-    e1 = [exp(im * 2π * k1 / n) for k1 in 0:n-1]
-    e2 = [exp(im * 2π * k2 / n) for k2 in 0:n-1]
-    
-    total_points = 4 * n^2 * (nx-1) * (ny-1)
-    x_vals = Vector{Float64}(undef, total_points)
-    y_vals = Vector{Float64}(undef, total_points)
-    z_vals = Vector{Float64}(undef, total_points)
-    
-    cos_a = cos(a)
-    sin_a = sin(a)
-    idx = 1
-    
-    @inbounds for k1 in 0:n-1
-        e1_k = e1[k1+1]
-        for k2 in 0:n-1
-            e2_k = e2[k2+1]
-            for i in 1:nx-1, j in 1:ny-1
-                points = [(i, j), (i+1, j), (i+1, j+1), (i, j+1)]
-                for (p_i, p_j) in points
-                    z1 = e1_k * U1_pow[p_i, p_j]
-                    z2 = e2_k * U3_pow[p_i, p_j]
-                    x_vals[idx] = real(z1)
-                    y_vals[idx] = real(z2)
-                    z_vals[idx] = imag(z1)*cos_a + imag(z2)*sin_a
-                    idx += 1
-                end
-            end
-        end
+function calabi_yau(n::Int, α::Float64; grid_res::Int64=50, color_palette::Symbol=:rainbow, color=nothing)
+    α = mod(α, 2π)
+    a_vals = range(0.0, π/2, length=grid_res)
+    b_vals = range(-π/2, π/2, length=grid_res)
+
+    U1 = [u1(a, b)^(2/n) for a in a_vals, b in b_vals]
+    U3 = [u3(a, b)^(2/n) for a in a_vals, b in b_vals]
+
+    plt = plot3d(legend=false, framestyle=:none, grid=false, axis=false, size=(800, 800), camera=(30, 30))
+
+    if isnothing(color)
+        colors = cgrad(color_palette, n^2, categorical=true)
+    else
+        colors = fill(color, n^2)
     end
 
-    plot3d(x_vals, y_vals, z_vals,
-           seriestype=:surface,
-           legend=false,
-           framestyle=:none,
-           xticks=:none,
-           yticks=:none,
-           zticks=:none)
+    phase1 = [exp(im * 2π * k1 / n) for k1 in 0:n-1]
+    phase2 = [exp(im * 2π * k2 / n) for k2 in 0:n-1]
+
+    cosα = cos(α)
+    sinα = sin(α)
+
+    idx = 1
+    for k1 in 0:n-1, k2 in 0:n-1
+        z1 = phase1[k1+1] .* U1
+        z2 = phase2[k2+1] .* U3
+
+        x = real.(z1)
+        y = real.(z2)
+        z = imag.(z1) .* cosα .+ imag.(z2) .* sinα
+
+        surface!(plt, x, y, z, color=isnothing(color) ? colors[idx] : color)
+        idx += 1
+    end
+    
+    return plt
+end
+
+function calabi_yau_anim(n::Int64; grid_res::Int64=50, anim_res::Int64=50, color_palette::Symbol=:rainbow, color=nothing)
+    angles = LinRange(0, 2π, anim_res + 1)[1:end-1]
+    calabi_yau(n, angles[1], grid_res=grid_res, color_palette=color_palette, color=color)
+    anim = @animate for (i, α) in enumerate(angles)
+        calabi_yau(n, α, grid_res=grid_res, color_palette=color_palette, color=color)
+    end
+    return anim
 end
 
 end # module
